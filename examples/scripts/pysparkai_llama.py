83df7c053679cf969b75bff9b7354b8b56467e17
PZD-CHINA <zedong.peng@intel.com>
2024-03-13 23:24:40 -0700

    new example

--- examples/scripts/pysparkai_llama.py
+++ examples/scripts/pysparkai_llama.py
@@ -11,5 +11,22 @@ spark_ai.activate()
 #auto_df = spark_ai.create_df("https://www.carpro.com/blog/full-year-2022-national-auto-sales-by-brand")
 #auto_df.show()
 
-df=spark_ai.create_df("https://en.wikipedia.org/wiki/List_of_presidents_of_the_United_States", ["president", "vice_president"])
-df.show()
+#df=spark_ai.create_df("https://en.wikipedia.org/wiki/List_of_presidents_of_the_United_States", ["president", "vice_president"])
+#df.show()
+df = spark_ai._spark.createDataFrame(
+    [
+        ("Normal", "Cellphone", 6000),
+        ("Normal", "Tablet", 1500),
+        ("Mini", "Tablet", 5500),
+        ("Mini", "Cellphone", 5000),
+        ("Foldable", "Cellphone", 6500),
+        ("Foldable", "Tablet", 2500),
+        ("Pro", "Cellphone", 3000),
+        ("Pro", "Tablet", 4000),
+        ("Pro Max", "Cellphone", 4500)
+    ],
+    ["product", "category", "revenue"]
+)
+
+df.ai.transform("What are the best-selling and the second best-selling products in every category?").show()
+
